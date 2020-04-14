f249e49a3e6d6e92a1784d1ae4b97577f3379b48
Harish Tagadghar <harishtgtg@gmail.com>
2020-04-14 17:49:25 +0530

    basic emit on socket function setup on user connection and disconnected

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-io()
+let socket =  io()
+
+socket.on('message' , (msg)=> {
+    console.log(msg);
+    
+})
+
+document.querySelector('#form').addEventListener('submit' , (e) => {
+    e.preventDefault()
+    let message = e.target.elements.message.value
+    socket.emit('sentmessage' , message)
+})
--- src/index.js
+++ src/index.js
@@ -11,9 +11,21 @@ let server = http.createServer(app)
 let io = socketio(server)
 app.use(express.static(publicPath))
 
-io.on('connection' , () => {
+io.on('connection' , (socket) => {
     console.log(`new connection`);
+
+    socket.emit('message' , 'Welcome!');
+
+    socket.broadcast.emit('message','a new user joined the group')
     
+    socket.on('sentmessage', (msg) => {
+        io.emit('message' , msg)
+    })
+
+    socket.on('disconnect' , () => {
+        io.emit('message' , 'a user left the group')
+    })
+
 })
 
 let port = process.env.PORT || 3000
